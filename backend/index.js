af7f58ce09949b1378e846556e6fd3fff29ddec5
om salunke <omsaunke343@gmail.com>
2024-10-13 11:14:06 +0530

    server created

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+
+import express from "express"
+import cookieParser  from "cookie-parser"
+import cors from "cors"
+const app = express();
+
+const PORT = 3000
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions={
+    origin:"http://localhost:5173",
+    credential:true
+};
+app.use(cors(corsOptions))
+app.listen(PORT,()=>{
+console.log("Server Running at port "+PORT)
+})
